143759cb5daf18d19bb568f648210fd02a37ca97
Nathan Haberman <na.haberman@gmail.com>
2017-09-19 10:04:52 -0500

    Added Likes and Books

---
+++ Django/Likes_and_Books/apps/liked_books/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    liked_users = models.ManyToManyField(User, related_name='liked_books')
+    uploader = models.ForeignKey(User, on_delete=None, related_name='uploaded_books')
